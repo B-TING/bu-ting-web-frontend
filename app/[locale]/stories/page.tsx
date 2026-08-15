5b8544b1209397dff23454946c0c7b2d0ef1f7dd
Kim Hyunji <jikim567@naver.com>
2026-08-15 22:57:24 +0900

    feat: 여행기 피드 API 연동

--- app/[locale]/stories/page.tsx
+++ app/[locale]/stories/page.tsx
@@ -1,8 +1,5 @@
 import { StoriesFeed } from '@/app/[locale]/stories/components/StoriesFeed';
-import { getStoryFeedItems } from '@/app/[locale]/stories/story-data';
 
 export default function StoriesPage() {
-  const stories = getStoryFeedItems();
-
-  return <StoriesFeed stories={stories} />;
+  return <StoriesFeed />;
 }
